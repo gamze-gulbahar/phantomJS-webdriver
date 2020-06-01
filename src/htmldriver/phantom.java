1234996019dc8a5f0c0e88fe78d1ff0b392e7730
gamze-gulbahar <65761867+gamze-gulbahar@users.noreply.github.com>
2020-06-01 23:16:07 +0300

    Update phantom.java

--- src/htmldriver/phantom.java
+++ src/htmldriver/phantom.java
@@ -12,65 +12,63 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class phantom {		
-		static String fileWithPath = "screenshots";
-		public static void getKargoPrice() throws Exception {
-			File file = new File("/Users/gamze/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");				
-            System.setProperty("phantomjs.binary.path", file.getAbsolutePath());		
-            WebDriver driver = new PhantomJSDriver();
-            driver.get("https://www.mediamarkt.com.tr/tr/product/_81ut00d8tx-%C4%B1deapad-s145-ryzen3-3200u-8gb-ram-256gb-ssd-vega3-graphics-15-6-hd-w10-laptop-silver-1209294.html");         
-            WebElement e = driver.findElement(By.xpath("//*[@id=\"pdp-add-to-cart\"]/span"));
-            takeSnapShot(driver,fileWithPath+ "/mediamarkt");
-            e.click();
-            Thread.sleep(5000);
-            WebElement t = driver.findElement(By.xpath("//*[@id=\"basket-flyout-cart\"]/span"));
-            takeSnapShot(driver,fileWithPath+ "/sepet");
-            //System.out.println(t.getText());
-            t.click();    
-            Thread.sleep(5000);
-           // System.out.println(driver.getPageSource());
-            List <WebElement> kargo = driver.findElements(By.cssSelector(".cototals-label.ng-binding"));
-            List <WebElement> value = driver.findElements(By.cssSelector(".cototal-value.ng-binding"));
+	static String fileWithPath = "screenshots";
+	public static void getKargoPrice() throws Exception {
+		File file = new File("/Users/gamze/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");				
+            	System.setProperty("phantomjs.binary.path", file.getAbsolutePath());		
+           	WebDriver driver = new PhantomJSDriver();
+            	driver.get("https://www.mediamarkt.com.tr/tr/product/_81ut00d8tx-%C4%B1deapad-s145-ryzen3-3200u-8gb-ram-256gb-ssd-vega3-graphics-15-6-hd-w10-laptop-silver-1209294.html");         
+            	WebElement e = driver.findElement(By.xpath("//*[@id=\"pdp-add-to-cart\"]/span"));
+            	takeSnapShot(driver,fileWithPath+ "/mediamarkt");
+            	e.click();
+            	Thread.sleep(5000);
+            	WebElement t = driver.findElement(By.xpath("//*[@id=\"basket-flyout-cart\"]/span"));
+            	takeSnapShot(driver,fileWithPath+ "/sepet");
+            	//System.out.println(t.getText());
+            	t.click();    
+            	Thread.sleep(5000);
+           	// System.out.println(driver.getPageSource());
+            	List <WebElement> kargo = driver.findElements(By.cssSelector(".cototals-label.ng-binding"));
+            	List <WebElement> value = driver.findElements(By.cssSelector(".cototal-value.ng-binding"));
             
-            takeSnapShot(driver,fileWithPath+ "/approvedsepet");
+            	takeSnapShot(driver,fileWithPath+ "/approvedsepet");
            
-            for(WebElement k:kargo) {
-            	if(k.getText().equals("Kargo Ücreti")) {
-            		System.out.println(k.getText() + ": " + value.get(1).getText() );
+            	for(WebElement k:kargo) {
+            		if(k.getText().equals("Kargo Ücreti")) {
+            			System.out.println(k.getText() + ": " + value.get(1).getText() );
             	}
             }
             
             driver.quit();
             
-		}
+	}
 		
-		public static void getIP() throws Exception {
+	public static void getIP() throws Exception {
 			
-			File file = new File("/Users/gamze/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");				
-            System.setProperty("phantomjs.binary.path", file.getAbsolutePath());		
-            WebDriver driver = new PhantomJSDriver();	
-            driver.get("http://httpbin.org/get");       
-            WebElement jsonEl = driver.findElement(By.tagName("pre"));
-            String json = jsonEl.getText();
-            JSONObject obj = new JSONObject(json);
-            String origin = obj.getString("origin");
+		File file = new File("/Users/gamze/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");				
+            	System.setProperty("phantomjs.binary.path", file.getAbsolutePath());		
+            	WebDriver driver = new PhantomJSDriver();	
+            	driver.get("http://httpbin.org/get");       
+            	WebElement jsonEl = driver.findElement(By.tagName("pre"));
+            	String json = jsonEl.getText();
+            	JSONObject obj = new JSONObject(json);
+            	String origin = obj.getString("origin");
             
-            takeSnapShot(driver,fileWithPath + "/json");
-            System.out.println(origin);
-			
-		}
+            	takeSnapShot(driver,fileWithPath + "/json");
+            	System.out.println(origin);
+	}
 		
-		public static void takeSnapShot(WebDriver driver,String fileWithPath) throws Exception{
+	public static void takeSnapShot(WebDriver driver,String fileWithPath) throws Exception{
 
-	        TakesScreenshot scrShot =((TakesScreenshot)driver);
+		TakesScreenshot scrShot =((TakesScreenshot)driver);
 	        File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
 	        File DestFile=new File(fileWithPath);
 	        FileUtils.copyFile(SrcFile, DestFile);
 
-	    }
-		public static void main(String[] args) throws Exception {
+	}
+	public static void main(String[] args) throws Exception {
         	        
-			getKargoPrice();
-            getIP();
-                                 			
-           }	
+		getKargoPrice();
+            	getIP();                      			
+        }	
 }
